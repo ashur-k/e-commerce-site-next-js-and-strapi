80dd74b0cb098118f9edf6d535c82be5edf76e0b
ashur-k <ashurkanwal@yahoo.com>
2021-06-15 14:19:57 +0100

    updated product urls, now each product get fetched from backend strapi app

--- ecommerce-frontend/pages/products/[slug].js
+++ ecommerce-frontend/pages/products/[slug].js
@@ -1,12 +1,9 @@
 import Head from 'next/head'
-import products from '../../products.json'
-import {fromImageToUrl} from '../../utils/urls'
+import { fromImageToUrl, API_URL } from '../../utils/urls'
 import { twoDecimals } from '../../utils/format'
 
 
-const product = products[0]
-
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <div>
             <Head>
@@ -26,10 +23,32 @@ const Product = () => {
         )
 }
 
+export async function getStaticProps({ params: { slug } }){
+    const product_res = await fetch(`${API_URL}/products/?slug=${slug}`)
+    const found = await product_res.json()
+
+    return {
+        props: {
+            //for the API filter response is 
+            //an array an we are choosing 1st array
+            product: found[0] 
+            
+        }
+    }
+}
+
 export async function getStaticPaths() {
     //Retrieve all the possible paths
+        const products_res = await fetch(`${API_URL}/products/`)
+        const products = await products_res.json()
 
     //Return them to NextJs content
+    return {
+        paths: products.map(product => ({
+            params: { slug: String(product.slug)}
+        })),
+        fallback: false // Tells to next.js 404 if the param is not found
+    }
 }
 
 export default Product
