25e199752e04c25c60c3df06723fdc32ccb768a1
ashur-k <ashurkanwal@yahoo.com>
2021-06-15 13:50:29 +0100

    created strapi app, created next.js app added product json file to display data

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bce3ddff9171277a3e681c62496d4695'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccc14010-93db-4cfe-8c09-a707afebba80'
+};
